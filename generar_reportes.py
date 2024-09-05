0c9ad99a8503ea2f37cef4a890ad290352db2d16
daancabo97 <danielcaicedo285@gmail.com>
2024-09-05 10:49:43 -0500

    Ajustar que el total de los servicios sean especificos por cada persona

--- generar_reportes.py
+++ generar_reportes.py
@@ -9,9 +9,12 @@ import numpy as np
 from pandas.tseries.offsets import BDay
 import warnings
 
-warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
-# Función para calcular días hábiles del cierre de los casos
+warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
+servicios_completos = ['Base de Datos', 'Catalogación', 'Funcional', 'Otro', 'Software Base', 'SysAdmin']
+
+
+# Calcular días hábiles del cierre de los casos
 def calcular_dias_habiles(fecha_inicio, fecha_fin):
         
         if pd.isnull(fecha_inicio) or pd.isnull(fecha_fin):
@@ -36,6 +39,7 @@ def ajustar_ancho_columnas(writer, sheet_name):
             ajustar_anchura = (max_length + 2)
             worksheet.column_dimensions[column].width = ajustar_anchura
 
+
 # Generar gráficas de barras
 def generar_grafica_barras(worksheet, data_range, categoria_range, titulo, celda):
             
@@ -45,10 +49,10 @@ def generar_grafica_barras(worksheet, data_range, categoria_range, titulo, celda
             chart.y_axis.title = 'Casos'
             
             data = Reference(worksheet, min_col=data_range['min_col'], min_row=data_range['min_row'],
-                            max_col=data_range['max_col'], max_row=data_range['max_row'])
+                    max_col=data_range['max_col'], max_row=data_range['max_row'])
             
             categories = Reference(worksheet, min_col=categoria_range['min_col'], min_row=categoria_range['min_row'],
-                                max_col=categoria_range['max_col'], max_row=categoria_range['max_row'])
+                        max_row=categoria_range['max_row']) 
             
             chart.add_data(data, titles_from_data=True)
             chart.set_categories(categories)
@@ -61,23 +65,34 @@ def generar_grafica_barras(worksheet, data_range, categoria_range, titulo, celda
 # Generar reporte en Excel
 def generar_reporte_excel(df, ruta_salida, total_casos, casos_bac, casos_cobis):
             
-            # Calcular días en la columna SLA para cada caso -> ('Casos Mas Demorados')
+
+        # Calcular días en la columna SLA para cada caso -> ('Casos Mas Demorados')
             df['Fecha/Hora Asignación'] = pd.to_datetime(df['Fecha/Hora Asignación'], errors='coerce')
             df['Fecha\nCierre'] = pd.to_datetime(df['Fecha\nCierre'], errors='coerce')
             df['Dias SLA'] = df.apply(lambda row: calcular_dias_habiles(row['Fecha/Hora Asignación'], row['Fecha\nCierre']), axis=1)
 
 
-            # Obtener estadísticas de los casos según días - columna SLA -> ('Casos Mas Demorados')
+        # Obtener estadísticas de los casos según días - columna SLA -> ('Casos Mas Demorados')
             casos_antes_tres_dias = df[df['Dias SLA'] <= 3].shape[0]
             casos_despues_tres_dias = df[df['Dias SLA'] > 3].shape[0]
 
-            # Caoturar demas casos correspondientes al archivo
+
+        # Capturar demas casos correspondientes al archivo
             casos_por_servicio = tabla_pivote(df, 'Servicio')
             casos_por_persona = tabla_pivote(df, 'Asignado a')
             casos_por_componente = tabla_pivote(df, 'Componente')
-            casos_por_ambiente = tabla_pivote(df, 'Ambiente') 
+            casos_por_ambiente = tabla_pivote(df, 'Ambiente')
 
-            # Obtener los casos que más tiempo han tomado en sacar (Topaz y BAC) -> ('Casos Mas Demorados')
+
+        # Calcular el total de servicios asignados por persona
+            casos_por_persona = df.groupby(['Asignado a', 'Servicio']).size().unstack(fill_value=0).reindex(columns=servicios_completos, fill_value=0)
+            casos_por_persona = casos_por_persona.astype(int)
+            casos_por_persona['Total'] = casos_por_persona.sum(axis=1)
+            casos_por_persona = casos_por_persona.reset_index()
+
+
+
+        # Obtener los casos que más tiempo han tomado en sacar (Topaz y BAC) -> ('Casos Mas Demorados')
             df['Duracion (minutos)'] = pd.to_numeric(df['Duracion (minutos)'], errors='coerce')
             casos_mas_demorados_topaz = df[df['USUARIO'].str.contains('topaz', case=False, na=False)].nlargest(5, 'Duracion (minutos)')
             casos_mas_demorados_bac = df[df['USUARIO'].str.contains('bac', case=False, na=False)].nlargest(5, 'Duracion (minutos)')
@@ -94,14 +109,16 @@ def generar_reporte_excel(df, ruta_salida, total_casos, casos_bac, casos_cobis):
                 ajustar_ancho_columnas(writer, 'Resumen')
                 casos_por_servicio.to_excel(writer, sheet_name='Casos por Servicio')
                 ajustar_ancho_columnas(writer, 'Casos por Servicio')
-                casos_por_persona.to_excel(writer, sheet_name='Casos por Persona')
+                casos_por_persona.to_excel(writer, sheet_name='Casos por Persona',  index=False)
                 ajustar_ancho_columnas(writer, 'Casos por Persona')
                 casos_por_componente.to_excel(writer, sheet_name='Casos por Componente')
                 ajustar_ancho_columnas(writer, 'Casos por Componente')
                 casos_por_ambiente.to_excel(writer, sheet_name='Casos por Ambiente')
                 ajustar_ancho_columnas(writer, 'Casos por Ambiente')
 
-                # Añadir gráfica de barras por seccion en el archivo excel
+
+
+            # Añadir gráfica de barras por seccion en el archivo excel
                 workbook = writer.book
 
                 hojas_con_graficas = {
@@ -126,17 +143,25 @@ def generar_reporte_excel(df, ruta_salida, total_casos, casos_bac, casos_cobis):
                         'max_col': 1,
                         'max_row': len(data) + 1
                     }
-                    generar_grafica_barras(worksheet, data_range, categoria_range, f'{nombre_hoja}', 'E5')
+                    if nombre_hoja == 'Casos por Persona':
+                        celda = 'I15'  
+                    else:
+                        celda = 'I6'
+                    generar_grafica_barras(worksheet, data_range, categoria_range, f'{nombre_hoja}', celda)
 
-                # Añadir los 5 casos más demorados (Topaz y BAC) -> ('Casos Mas Demorados')
-                # Casos BAC
+
+
+            # Añadir los 5 casos más demorados (Topaz y BAC) -> ('Casos Mas Demorados')
+
+            # Casos BAC
                 startrow_topaz = 0
                 casos_mas_demorados_topaz.to_excel(writer, sheet_name='Casos Más Demorados', index=False, startrow=startrow_topaz)
-                # Casos BAC
+             # Casos BAC
                 startrow_bac = startrow_topaz + len(casos_mas_demorados_topaz) + 2
                 casos_mas_demorados_bac.to_excel(writer, sheet_name='Casos Más Demorados', index=False, startrow=startrow_bac)
                 
-                # Añadir las estadísticas de casos cerrados en <= 3 días y > 3 días -> ('Casos Mas Demorados')
+
+            # Añadir las estadísticas de casos cerrados en <= 3 días y > 3 días -> ('Casos Mas Demorados')
                 worksheet = writer.sheets['Casos Más Demorados']
                 stats_start_row = startrow_bac + len(casos_mas_demorados_bac) + 2
                 worksheet.cell(row=stats_start_row, column=1, value="Casos Cerrados antes de 3 días")
@@ -144,7 +169,8 @@ def generar_reporte_excel(df, ruta_salida, total_casos, casos_bac, casos_cobis):
                 worksheet.cell(row=stats_start_row, column=2, value="Casos Cerrados mayor a 3 días")
                 worksheet.cell(row=stats_start_row + 1, column=2, value=casos_despues_tres_dias)
 
-                # Aplicar bordes y negrita a los títulos de las estadísticas -> ('Casos Mas Demorados')
+
+            # Aplicar bordes y negrita a los títulos de las estadísticas -> ('Casos Mas Demorados')
                 thin_border = Border(left=Side(style='thin'), 
                                     right=Side(style='thin'), 
                                     top=Side(style='thin'), 
